a21c1aba83d9010b02596b15e5b6850dd68fb7f0
Yash Patel <pately28@mcmaster.ca>
2024-01-31 20:11:18 -0500

    Added my files

---
+++ FE_Dashboard/Yash's Screens/Intro_Screen/TouchGFX/build/MINGW32_NT-6.2/generated/images/src/image_soc.d
@@ -0,0 +1,5 @@
+build/MINGW32_NT-6.2/generated/images/src/image_soc.o: \
+ generated/images/src/image_soc.cpp \
+ ../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+
+../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
